8d022ba1521bae451461014eb20d818b0f91c7cd
Alve <alauddinalveee@gmail.com>
2026-03-04 05:15:09 +0600

    Checking even or odd

--- p3.java
+++ p3.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class p3 {
     public static void main(String[] args) {
         System.out.println("Enter a Number: ");
-        Scanner s = new Scanner(System.in);
-        int num = s.nextInt();
-        s.close();
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
         if(num%2==1)
         {
             System.out.println("The number is odd");
