49a929760d8cbdb3269d9c62e0030c236326004b
Alve <alauddinalveee@gmail.com>
2026-03-04 03:39:47 +0600

    p1.java

---
+++ p1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.util.Scanner;
+public class p1 {
+    public static void main(String[] args) {
+        //System.out.println("Hello, World");
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Name: ");
+        String name = s.nextLine();
+        s.close();
+        System.out.print("Hello, "+name + "!");
+    }
+}
